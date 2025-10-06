d364eb46381ff06edcf53f8d361381f798fe3095
Jirawas229 <68070229@kmitl.ac.th>
2025-10-06 20:25:35 +0700

    javascript

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
-const myinput = document.getElementById("myinput")
+let currenin ;
+let currenout ;
+let currenacc ;
+let currencash ;
+let bankopin ;
 
-function add() {
-    myinput.value = Number(myinput.value) + 1
+document.getElementById("Submitconvert").onclick = function(){
+    currenin = Number(document.getElementById("in3").value) ;
+    if(document.getElementById("se_2").value =="USD"){
+        currenout = currenin*32.
+        document.getElementById("in4").value = `${currenout.toFixed(2)} THB`
+    } else {
+        currenout = currenin/32.43
+        document.getElementById("in4").value = `${currenout.toFixed(2)} USD`
+    }
+}
+
+document.getElementById("balanceinformation").onclick = function(){
+    currenacc = Number(document.getElementById("in1").value)
+    currencash = Number(document.getElementById("in2").value)
+    document.getElementById("preview").value = `Your account balance is ${currenacc}\nYour cash balance is ${currencash}`
+} 
+
+document.getElementById("BankOp").onclick = function(){
+    bankopin = Number(document.getElementById("BankOpIn").value)
+    if(document.getElementById("BankOpType").value == "Deposit"){
+        currenacc += bankopin
+        currencash -= bankopin
+        document.getElementById("preview").value = `Your account balance is ${currenacc}\nYour cash balance is ${currencash}`
+    } else {
+        currenacc -= bankopin
+        currencash += bankopin
+        document.getElementById("preview").value = `Your account balance is ${currenacc}\nYour cash balance is ${currencash}`
+    }
 }
