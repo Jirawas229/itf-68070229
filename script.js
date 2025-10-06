8a6a2ec522e067408f8ff639c663cbb6ee7aaa56
Jirawas229 <68070229@kmitl.ac.th>
2025-10-06 21:05:20 +0700

    ja2.1

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ document.getElementById("BankOp").onclick = function(){
         currenacc += bankopin
         currencash -= bankopin
         document.getElementById("premon").value = `Your account balance is ${currenacc}\nYour cash balance is ${currencash}`}
-        document.getElementById("preview").value += `You withdraw ${bankopin} Baht\n`
+        document.getElementById("preview").value += `You deposit ${bankopin} Baht\n`
     } else {
         if(currenacc < bankopin){
             window.alert("You don't have enough money to withdraw")
